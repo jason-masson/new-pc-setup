791371f0c9a175f3fd4a943993e3d35ea9a2e3aa
Jason Masson <69549693+jason-masson@users.noreply.github.com>
2021-10-27 09:24:32 -0400

    Update Winget-Install.ps1

--- ps/Winget-Install.ps1
+++ ps/Winget-Install.ps1
@@ -1,19 +1,27 @@
-﻿#clear console
-cls
+﻿#Install KB -- https://docs.microsoft.com/en-us/windows/package-manager/winget/install
+#VMware Workstation Prereqs -- https://kb.vmware.com/s/article/55798
+
+#clear console
+Clear-Host
 
 #start time
 Get-Date 
 
 #variable for the packages
-$packages = 'TechSmith.Snagit' , 'Microsoft.Teams' , 'Microsoft.Office' , 'BraveSoftware.BraveBrowser' , 'Devolutions.RemoteDesktopManagerFree' , 'Jabra.Direct' , 'Notepad++.Notepad++' , 'VideoLAN.VLC' , 'WinSCP.WinSCP' , '7zip.7zip'
+$packages = '7zip.7zip',
+            'BraveSoftware.BraveBrowser',
+            'Devolutions.RemoteDesktopManagerFree',
+            'Jabra.Direct',
+            'Microsoft.Teams', 
+            'Microsoft.Office',
+            'Notepad++.Notepad++', 
+            'TechSmith.Snagit',
+            'VideoLAN.VLC',
+            'WinSCP.WinSCP'
 
 #install loop
-#Install KB -- https://docs.microsoft.com/en-us/windows/package-manager/winget/install
-#VMware Workstation -- https://kb.vmware.com/s/article/55798
 foreach ($package in $packages) {
-
-winget install $package -e --force
-
+    winget install $package -e --force
 }
 
 #end time
